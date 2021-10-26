a6baef5a74b73ff2eafc3c1b7a781dec4c578163
Flopsey <25252925+Flopsey@users.noreply.github.com>
2021-10-26 17:35:19 +0200

    Add livecheck and zap stanzas to Migrosbank E-Banking (#2)

--- Casks/migros-bank-e-banking.rb
+++ Casks/migros-bank-e-banking.rb
@@ -9,7 +9,19 @@ cask "migros-bank-e-banking" do
   desc "E-banking app"
   homepage "https://www.migrosbank.ch/de/privatpersonen/e-banking.html"
 
+  livecheck do
+    url "https://www.migrosbank.ch/de/e-banking-update.html"
+    strategy :page_match
+    regex(%r{href=.*?/MigrosBank-EBanking-Mac-(\d+(?:\.\d+)*)\.pkg}i)
+  end
+
   pkg "MigrosBank-EBanking-Mac-#{version}.pkg"
 
   uninstall pkgutil: "com.kobil.midappqt.PROD"
+
+  zap trash: [
+    "~/Library/Caches/com.kobil.midAppMacOS",
+    "~/Library/Preferences/com.kobil.midAppMacOS.plist",
+    "~/Library/Saved Application State/com.kobil.midAppMacOS.savedState",
+  ]
 end
